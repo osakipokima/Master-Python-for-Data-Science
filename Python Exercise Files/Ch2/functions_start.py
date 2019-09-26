a998e167a4f207f9b3a415967de8713e2834a39d
Osaki Pokima <opokima5061@floridapol.yedu>
2019-09-26 15:23:40 -0400

    Completed couple of lessons on variables and function calls

--- Python Exercise Files/Ch2/functions_start.py
+++ Python Exercise Files/Ch2/functions_start.py
@@ -3,16 +3,43 @@
 #
 
 # define a basic function
-
+def func1():
+    print("I am a function. I pull up to the function")
 
 # function that takes arguments
+def func2(arg1, arg2):
+    print(arg1, " ", arg2)
 
 
 # function that returns a value
-
+def cube(x):
+    return x*x*x
 
 # function with default value for an argument
-
+def power(num, x=1):
+    result = 1
+    for i in range(x):
+        result = result * num
+    return result
 
 #function with variable number of arguments
+def multi_add(*args):
+    result = 0
+    for x in args:
+        result = result + x
+    return result
+
+# func1()  #function is called directly executes call of the function
+# print(func1()) #function is called within the print function
+
+# #since we didn't call the function in print, it prints none and the value of the function itself
+# #the output is an example of how functions are objects
+# print (func1) 
+# func2 (10,20)
+# print (func2(10,20))
+# print (cube(3))
+print(power(2))
+print(power(2,3))
+print(power(x=3, num=2))
+print(multi_add(4,5,6,12,8))
 
--- Python Exercise Files/Ch2/variables_start.py
+++ Python Exercise Files/Ch2/variables_start.py
@@ -1,15 +1,31 @@
-# 
-# Example file for variables
-#
+# # 
+# # Example file for variables
+# #
 
-# Declare a variable and initialize it
+# # Declare a variable and initialize it
+f=0
+# print(f)
 
 
-# # re-declaring the variable works
+# # # re-declaring the variable works
+# f="abc"
+# print(f)
 
-
-# # ERROR: variables of different types cannot be combined
-
+# # # ERROR: variables of different types cannot be combined
+# print("this is a string " + str(123))
 
 # Global vs. local variables in functions
+def someFunction():
+    # "def" gets printed since we called global and so will print(f) at the end
+    global f
+    f = "def"
+    print(f)
+# def will be printed first from the local variable inside the function
+someFunction()
+# 0 will be printed since f is a global variable defiend
+print(f)
+
+del f
+print(f)
+
 
