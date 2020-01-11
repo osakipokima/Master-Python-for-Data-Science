f65b2283caeca9366e41e16517e93a6239f05b03
osaki pokima <osakipokima@gmail.com>
2020-01-10 20:33:57 -0500

    finished up chapter 4 with built in python modules and os import

--- Python Exercise Files/Ch4/files_start.py
+++ Python Exercise Files/Ch4/files_start.py
@@ -4,19 +4,27 @@
 
 def main():  
   # Open a file for writing and create it if it doesn't exist
-
+  # f = open("textfile.txt", "w+")
+  f = open("textfile.txt", "r")
 
   # Open the file for appending text to the end
 
 
   # write some lines of data to the file
+  #for i in range(10):
+    # f.write("This is line " + str(i) + "\r\n")
 
   
   # close the file when done
-
+  # f.close()
   
   # Open the file back up and read the contents
-
+  if f.mode == 'r':
+    # contents = f.read()
+    fl = f.readlines()
+    for x in fl:
+      print (x)
+    #print(contents)
     
 if __name__ == "__main__":
   main()
